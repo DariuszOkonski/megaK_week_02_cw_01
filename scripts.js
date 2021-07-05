356b8e5e05558cc489aad4ecf8a5e73a915465b1
Dariusz Okonski <darek200180@gmail.com>
2021-07-05 12:29:23 +0100

    add displaying in browser

--- scripts.js
+++ scripts.js
@@ -33,19 +33,37 @@ const getCorrectNumber = (message) => {
     return Number(number);
 }
 
+const sendResultToHTML = (firstNumber, secondNumber, multiplication, addition, subtraction, division) => {
+    document.body.innerHTML = `
+        <h2>Your numbers: ${firstNumber}, ${secondNumber}</h2>
+        <p>-----------------------------------</p>
+        <p>multiplication: ${multiplication}</p>
+        <p>addition: ${addition}</p>
+        <p>subtraction: ${subtraction}</p>
+        <p>division: ${(isNaN(division) ? "Can't divide by 0" : division)}</p>
+    `;
+}
+
+const sendResultToConsole =(firstNumber, secondNumber, multiplication, addition, subtraction, division) => {
+    console.log(`Your numbers: ${firstNumber}, ${secondNumber}`)
+    console.log('-----------------------------------');
+    console.log("multiplication: " + multiplication);
+    console.log("addition: " + addition);
+    console.log("subtraction: " + subtraction)
+    console.log("division: " + (isNaN(division) ? "Can't divide by 0" : division));
+}
+
 const main = () => {
     const firstNumber = getCorrectNumber("first");
     const secondNumber = getCorrectNumber("second");
 
-    console.log(`Your numbers: ${firstNumber}, ${secondNumber}`)
-    console.log('-----------------------------------');
-
-    console.log("multiplication: " + multiplication(firstNumber, secondNumber));
-    console.log("addition: " + addition(firstNumber, secondNumber));
-    console.log("subtraction: " + subtraction(firstNumber, secondNumber))
-
+    const multiplicationResult = multiplication(firstNumber, secondNumber);
+    const additionResult = addition(firstNumber, secondNumber);
+    const subtractionResult = subtraction(firstNumber, secondNumber);
     const divisionResult = division(firstNumber, secondNumber);
-    console.log("division: " + (isNaN(divisionResult) ? "Can't divide by 0" : divisionResult));
+
+    sendResultToConsole(firstNumber, secondNumber, multiplicationResult, additionResult, subtractionResult, divisionResult)
+    sendResultToHTML(firstNumber, secondNumber, multiplicationResult, additionResult, subtractionResult, divisionResult)
 }
 
 main()
